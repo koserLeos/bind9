0a65d083f65b8c19967794aca3fddaddb3a58b69
Mark Andrews <marka@isc.org>
2019-03-06 17:45:03 +1100

    check deny named + grant subdomain

--- bin/tests/system/nsupdate/tests.sh
+++ bin/tests/system/nsupdate/tests.sh
@@ -1165,6 +1165,34 @@ EOF
   grep "status: NXDOMAIN" dig.out.ns10.test$n > /dev/null || ret=1
   [ $ret = 0 ] || { echo_i "failed"; status=1; }
 
+  n=`expr $n + 1`
+  ret=0
+  echo_i "check 'grant' in deny name + grant subdomain ($n)"
+  $NSUPDATE << EOF > nsupdate.out-$n 2>&1 || ret=1
+  key hmac-sha256:subkey 1234abcd8765
+  server 10.53.0.9 ${PORT}
+  zone denyname.example
+  update add foo.denyname.example 3600 IN TXT added
+  send
+EOF
+  $DIG $DIGOPTS +tcp @10.53.0.9 foo.denyname.example TXT > dig.out.ns9.test$n
+  grep "added" dig.out.ns9.test$n > /dev/null || ret=1
+  [ $ret = 0 ] || { echo_i "failed"; status=1; }
+
+  n=`expr $n + 1`
+  ret=0
+  echo_i "check 'deny' in deny name + grant subdomain ($n)"
+  $NSUPDATE << EOF > nsupdate.out-$n 2>&1 && ret=1
+  key hmac-sha256:subkey 1234abcd8765
+  server 10.53.0.9 ${PORT}
+  zone denyname.example
+  update add denyname.example 3600 IN TXT added
+  send
+EOF
+  $DIG $DIGOPTS +tcp @10.53.0.9 denyname.example TXT > dig.out.ns9.test$n
+  grep "added" dig.out.ns9.test$n > /dev/null && ret=1
+  [ $ret = 0 ] || { echo_i "failed"; status=1; }
+
 fi
 #
 #  Add client library tests here
