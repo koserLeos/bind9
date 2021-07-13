204ed24f4fcccc1e27e6d5175013b481c3193087
Mark Andrews <marka@isc.org>
2021-07-13 13:28:00 +1000

    Increase the TCP timeout allowance from 1 second to 3
    When starting a new TCP query, the initial SRTT estimate
    includes time for the kernel to resend SYN if necessary. At
    one second, the margin was not always sufficient; this commit
    increases it to three.

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -1947,12 +1947,12 @@ fctx_query(fetchctx_t *fctx, dns_adbaddrinfo_t *addrinfo,
 	srtt = addrinfo->srtt;
 
 	/*
-	 * Allow an additional second for the kernel to resend the SYN
+	 * Allow an additional 3 seconds for the kernel to resend the SYN
 	 * (or SYN without ECN in the case of stupid firewalls blocking
 	 * ECN negotiation) over the current RTT estimate.
 	 */
 	if ((options & DNS_FETCHOPT_TCP) != 0) {
-		srtt += US_PER_SEC;
+		srtt += 3 * US_PER_SEC;
 	}
 
 	/*
