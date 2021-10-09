52f448836c64d6f0003cc248f870fc38ab808425
Ondřej Surý <ondrej@sury.org>
2021-10-09 10:19:17 +0200

    Improve the clang-format and clang-tidy integration
    1. Update util/run-clang-tidy.py to clang-tidy-15 version
    
       The util/run-clang-tidy.py contains extra code that fixes up the file
       paths, so run-clang-tidy actually works.  The problem is that the
       run-clang-tidy expects to work from a "base" directory and strips
       directory from the compilation database.  Our copy of
       run-clang-tidy.py contains a little path fixer.
    
       Our custom version also doesn't try to apply fixes if the clang-tidy
       invocation failed with an error.
    
    2. Add util/run-clang-tidy.sh script that can be used to run clang-tidy
       on the compilation database.  The compilation database can be created
       by running:
    
        pip3 install compiledb
        autoreconf -fi
        ./configure --enable-developer
        make bind.keys.h
        (cd lib/dns && \
         make gen include/dns/enumtype.h \
                  include/dns/enumclass.h \
                  include/dns/rdatastruct.h \
                  code.h)
        (make -nwk all || true) | compiledb

---
+++ util/run-clang-tidy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+CLANG_TIDY_CHECKS="-*"
+CLANG_TIDY_CHECKS="$CLANG_TIDY_CHECKS,readability-braces-around-statements"
+CLANG_TIDY_CHECKS="$CLANG_TIDY_CHECKS${*:+,}${*}"
+
+python3 ./util/run-clang-tidy.py \
+	-clang-tidy-binary "${CLANG_TIDY:-clang-tidy-15}" \
+	-clang-apply-replacements-binary "${CLANG_APPLY_REPLACEMENTS:-clang-apply-replacements-15}" \
+	-checks="$CLANG_TIDY_CHECKS" \
+	-j 9 \
+	-fix \
+	-quiet
