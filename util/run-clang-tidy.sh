9f49b0a845a3f2ab16dfdb5c09b13660ae67938b
Ondřej Surý <ondrej@sury.org>
2021-10-14 11:13:08 +0200

    Add clang-tidy readability-uppercase-literal-suffix check
    Detects when the integral literal or floating point (decimal or
    hexadecimal) literal has a non-uppercase suffix and provides a fix-it
    hint with the uppercase suffix.

--- util/run-clang-tidy.sh
+++ util/run-clang-tidy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 CLANG_TIDY_CHECKS="-*"
-CLANG_TIDY_CHECKS="$CLANG_TIDY_CHECKS,readability-redundant-control-flow"
 CLANG_TIDY_CHECKS="$CLANG_TIDY_CHECKS,bugprone-macro-parentheses"
 CLANG_TIDY_CHECKS="$CLANG_TIDY_CHECKS,readability-braces-around-statements"
+CLANG_TIDY_CHECKS="$CLANG_TIDY_CHECKS,readability-redundant-control-flow"
+CLANG_TIDY_CHECKS="$CLANG_TIDY_CHECKS,readability-uppercase-literal-suffix"
 CLANG_TIDY_CHECKS="$CLANG_TIDY_CHECKS${*:+,}${*}"
 
 python3 ./util/run-clang-tidy.py \
