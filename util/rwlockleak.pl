fd8704a0baf10ad4b6ea319922b45dd1818fa13d
Ondřej Surý <ondrej@sury.org>
2021-12-06 11:23:56 +0100

    Add simple leak check scripts for mutex and rwlock

---
+++ util/rwlockleak.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+#
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
+
+# Massage the output from ISC_MEM_DEBUG to extract mem_get() calls
+# with no corresponding mem_put().
+
+while (<>) {
+    $gets{$1} = $_ if (/rwlock:init (?:0x)?([0-9a-f]+) func/);
+    delete $gets{$1} if /rwlock:destroy (?:0x)?([0-9a-f]+) func/;
+}
+print join('', values %gets);
+
+exit(0);
