00fb6aac58a4e34452c1f1ef48414558999f1762
Ondřej Surý <ondrej@isc.org>
2021-12-09 13:23:09 +0100

    WIP: Make the isc_astack growable and start low

--- lib/isc/include/isc/astack.h
+++ lib/isc/include/isc/astack.h
@@ -17,9 +17,10 @@
 #include <isc/types.h>
 
 isc_astack_t *
-isc_astack_new(isc_mem_t *mctx, size_t size);
+isc_astack_new(isc_mem_t *mctx, size_t min_size, size_t max_size);
 /*%<
- * Allocate and initialize a new array stack of size 'size'.
+ * Allocate and initialize a new bounded array stack.  Initially, min_size
+ * entries will be allocated, but would grow up to max_size entries.
  */
 
 void
