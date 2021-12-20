2d0c192a48567338404d8956b915d3c53207bc8f
Mark Andrews <marka@isc.org>
2021-12-20 16:38:34 +1100

    unknown: Use FIPS compatible key size
    RSA key sizes in FIPS mode must be at least 2048 bits.

--- bin/tests/system/unknown/ns3/sign.sh
+++ bin/tests/system/unknown/ns3/sign.sh
@@ -16,5 +16,5 @@
 zone=example
 rm -f K${zone}.+*+*.key
 rm -f K${zone}.+*+*.private
-keyname=`$KEYGEN -q -a RSASHA1 -b 1024 -n zone $zone`
-keyname=`$KEYGEN -q -a RSASHA1 -b 1024 -n zone -f KSK $zone`
+keyname=`$KEYGEN -q -a RSASHA256 -b 2048 -n zone $zone`
+keyname=`$KEYGEN -q -a RSASHA256 -b 2048 -n zone -f KSK $zone`
