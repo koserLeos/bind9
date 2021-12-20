091f68aa365dec1255f73b41673521c4421ea959
Mark Andrews <marka@isc.org>
2021-12-20 17:42:52 +1100

    pending: use FIPS compatible algorithms and key sizes
    RSASHA1 is verify only in FIPS mode, replace RSASHA1 uses with
    RSASHA256.  Additionally the minimum RSA key size permitted is 2048
    bits.  Adjusted key sizes to match.

--- bin/tests/system/pending/ns1/sign.sh
+++ bin/tests/system/pending/ns1/sign.sh
@@ -22,7 +22,7 @@ zonefile=root.db
 cp ../ns2/dsset-example. .
 cp ../ns2/dsset-example.com. .
 
-keyname1=`$KEYGEN -q -a RSASHA256 -b 1024 -n zone $zone`
+keyname1=`$KEYGEN -q -a RSASHA256 -b 2048 -n zone $zone`
 keyname2=`$KEYGEN -q -a RSASHA256 -b 2048 -f KSK -n zone $zone`
 cat $infile $keyname1.key $keyname2.key > $zonefile
 
