bf6192e4e052ae3c97f4b9f248053e67df0266da
Mark Andrews <marka@isc.org>
2022-04-01 14:27:47 +1100

    pending: use $DEFAULT_ALGORITHM instead rsasha256

--- bin/tests/system/pending/ns2/sign.sh
+++ bin/tests/system/pending/ns2/sign.sh
@@ -18,8 +18,8 @@ for domain in example example.com; do
 	infile=${domain}.db.in
 	zonefile=${domain}.db
 
-	keyname1=`$KEYGEN -q -a RSASHA256 -b 2048 -n zone $zone`
-	keyname2=`$KEYGEN -q -a RSASHA256 -b 2048 -f KSK -n zone $zone`
+	keyname1=$($KEYGEN -q -a ${DEFAULT_ALGORITHM} -n zone $zone)
+	keyname2=$($KEYGEN -q -a ${DEFAULT_ALGORITHM} -f KSK -n zone $zone)
 
 	cat $infile $keyname1.key $keyname2.key > $zonefile
 
