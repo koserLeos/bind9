6bbf6d4838a9a8dceea7d648fd28addc7e2d2827
Ondřej Surý <ondrej@isc.org>
2024-02-20 08:50:58 +0100

    Remove expired rdataset headers from the heap
    It was discovered that an expired header could sit on top of the heap
    a little longer than desireable.  Remove expired headers (headers with
    rdh_ttl set to 0) from the heap completely, so they don't block the next
    TTL-based cleaning.

--- lib/dns/rbtdb.c
+++ lib/dns/rbtdb.c
@@ -337,6 +337,12 @@ dns__rbtdb_setttl(dns_slabheader_t *header, dns_ttl_t newttl) {
 	} else {
 		isc_heap_decreased(header->heap, header->heap_index);
 	}
+
+	if (newttl == 0) {
+		INSIST(header->heap_index != 0);
+		isc_heap_delete(heap, header->heap_index);
+		header->heap_index = 0;
+	}
 }
 
 static bool
