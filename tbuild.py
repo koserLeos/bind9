4c36408f34c59a684abf8390aac8879c75af66ae
Petr Špaček <pspacek@isc.org>
2024-04-22 13:14:33 +0200

    WIP: mctx + de/compression

--- tbuild.py
+++ tbuild.py
@@ -7,11 +7,31 @@ ffibuilder = FFI()
 # globals needed to use the shared object. It must be in valid C syntax.
 ffibuilder.cdef(
     """
-typedef ... dns_name;
-typedef ... dns_name_t;
+typedef int... isc_result_t;
+typedef ... isc_mem_t;
+
+typedef struct { ...; } dns_compress_t;
+typedef int... dns_decompress_t;
+
+typedef struct { ...; } dns_name_t;
+typedef struct { ...; } dns_fixedname_t;
+
+void
+isc__mem_create(isc_mem_t **);
+
+void
+isc_mem_attach(isc_mem_t *, isc_mem_t **);
+
+isc_result_t
+dns_name_fromstring(dns_name_t *target, const char *src,
+		    const dns_name_t *origin, unsigned int options,
+		    isc_mem_t *mctx);
 
 static inline void
 dns_name_init(dns_name_t *name, unsigned char *offsets);
+
+dns_name_t *
+dns_fixedname_initname(dns_fixedname_t *fixed);
 
 uint32_t
 dns_name_hash(const dns_name_t *name);
@@ -25,7 +45,10 @@ dns_name_hash(const dns_name_t *name);
 ffibuilder.set_source(
     "_pi_cffi",
     """
-     #include "dns/name.h"   // the C header of the library
+     #include "isc/mem.h"
+     #include "dns/name.h"
+     #include "dns/compress.h"
+     #include "dns/fixedname.h"
 """,
     libraries=["dns"],
     include_dirs=["lib/dns/include", "lib/isc/include"],
