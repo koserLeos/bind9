8f908624b5d62d69a568576dfeb8e1e4030980e1
Petr Špaček <pspacek@isc.org>
2024-04-24 18:18:48 +0200

    WIP: broken values_agree()

--- tests/dns/qp_test.py
+++ tests/dns/qp_test.py
@@ -103,23 +103,32 @@ def test_fromname_toname_roundtrip(pyname_source: dns.name.Name) -> None:
 
 
 class QPIterator:
-    def __init__(self, qp, model):
-        self.qp = qp
+    def __init__(self, testcase):
+        self.testcase = testcase
+        self.iter_generation = testcase.generation
+        self.qp = testcase.qp
         self.citer = ffi.new("dns_qpiter_t *")
+        print(isclibs.dns_qpiter_init)
         isclibs.dns_qpiter_init(self.qp, self.citer)
-        self.expected = sorted(model.items())
+        self.expected = sorted(testcase.model.items())
 
     def _step(self, cfunc):
         iscname = ISCName()
         pval_r = ffi.new("void **")
         ival_r = ffi.new("uint32_t *")
         ret = cfunc(self.citer, iscname.cobj, pval_r, ival_r)
+        print("_step", cfunc, "returned", ret)
         if ret == isclibs.ISC_R_NOMORE:
-            raise StopIteration
+            raise StopIteration(cfunc)
         elif ret == isclibs.ISC_R_FAILURE:
-            raise RuntimeError
+            raise RuntimeError(cfunc)
+        print("  -> iterator returned: ", iscname.pyname(), pval_r[0], ival_r[0])
         assert ret == isclibs.ISC_R_SUCCESS
         return iscname, pval_r[0], ival_r[0]
+
+    def is_valid(self):
+        """Check if QP this iterator referenced is supposed to be still valid"""
+        return self.iter_generation == self.testcase.generation
 
     def next_(self):
         return self._step(isclibs.dns_qpiter_next)
@@ -134,6 +143,8 @@ class QPIterator:
 class BareQPTest(RuleBasedStateMachine):
     def __init__(self):
         super().__init__()
+        self.generation = 0
+        print("TEST RESTART FROM SCRATCH, GENERATION", self.generation)
 
         self.qpptr = ffi.new("dns_qp_t **")
         isclibs.dns_qp_create(MCTX, ffi.addressof(isclibs.qp_methods), NULL, self.qpptr)
@@ -143,9 +154,16 @@ class BareQPTest(RuleBasedStateMachine):
 
     names = Bundle("names")
 
+    def invalidate_refs(self):
+        """Mark current QP as changed - iterators which depend on unchanged state are now invalid"""
+        self.generation += 1
+        print("GENERATION ", self.generation)
+
     @rule(target=names, pyname=dns_names())
     def add(self, pyname):
         hypothesis.event("ADD")
+        print("ADD", pyname)
+        self.invalidate_refs()
 
         iscname = ISCName(pyname)
 
@@ -161,6 +179,8 @@ class BareQPTest(RuleBasedStateMachine):
     @rule(pyname=names)
     def delete(self, pyname):
         hypothesis.event("DELETE")
+        print("DELETE", pyname)
+        self.invalidate_refs()
         exists = pyname in self.model
 
         iscname = ISCName(pyname)
@@ -174,29 +194,57 @@ class BareQPTest(RuleBasedStateMachine):
         else:
             assert ret == isclibs.ISC_R_NOTFOUND
 
+    def _iterate_compare(self, tmp_iter, expected, stepfunc):
+        """QP must be non-empty, and must be positioned on first item"""
+
+        got_iscname, got_cobj, _ = tmp_iter.current()
+        qp_count = 1
+        for exp_pyname, exp_iscname in expected:
+            assert exp_pyname == got_iscname.pyname()
+            assert exp_iscname.cobj == ffi.cast("dns_name_t *", got_cobj)
+
+            try:
+                got_iscname, got_cobj, _ = stepfunc()
+                qp_count += 1
+            except StopIteration:
+                pass
+
+        with pytest.raises(StopIteration):
+            # QP must not have more items than we recorded in model
+            stepfunc()
+
+        assert qp_count == len(
+            tmp_iter.expected
+        ), "number of keys during full forward iteration must match"
+
     @rule()
     def values_agree(self):
-        hypothesis.event("CHECK", len(self.model))
-        tmp_iter = QPIterator(self.qp, self.model)
-        qp_count = 0
+        """Iterate through all values and check ordering"""
+        tmp_iter = QPIterator(self)
+        hypothesis.event("CHECK", len(tmp_iter.expected))
 
         # not-yet positioned iterator must always fail
         with pytest.raises(RuntimeError):
             got_iscname, got_cobj, _ = tmp_iter.current()
 
-        for exp_pyname, exp_iscname in tmp_iter.expected:
-            try:
-                got_iscname, got_cobj, _ = tmp_iter.next_()
-                qp_count += 1
-            except StopIteration:
-                pass
-            else:
-                assert exp_pyname == got_iscname.pyname()
-                assert exp_iscname.cobj == ffi.cast("dns_name_t *", got_cobj)
+        # prev() gives the last entry unless the QP is empty
+        try:
+            got_iscname, got_cobj, _ = tmp_iter.prev()
+        except StopIteration:
+            assert len(tmp_iter.expected) == 0
 
-        assert qp_count == len(
-            tmp_iter.expected
-        ), "number of keys during full iteration must match"
+        # try next() will fail if the set is empty or has only one item
+        try:
+            got_iscname, got_cobj, _ = tmp_iter.next_()
+        except StopIteration:
+            assert len(tmp_iter.expected) <= 1
+            if len(tmp_iter.expected) == 0:
+                return
+
+        self._iterate_compare(tmp_iter, sorted(tmp_iter.expected), tmp_iter.next_)
+        self._iterate_compare(
+            tmp_iter, sorted(tmp_iter.expected, reverse=True), tmp_iter.prev
+        )
 
 
 TestTrees = BareQPTest.TestCase
