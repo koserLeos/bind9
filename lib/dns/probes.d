7459d1392e2a37321112d74ad10b528010ab9d23
Aydın Mercan <aydin@isc.org>
2024-05-23 10:29:03 +0300

    add tracing points for qp
    Create USDT probes to get scoped and tailored data from qp operations.

--- lib/dns/probes.d
+++ lib/dns/probes.d
@@ -12,6 +12,38 @@
  */
 
 provider libdns {
+	probe qp_compact_done(void *, uint32_t, uint32_t, uint32_t, uint32_t);
+	probe qp_compact_start(void *, uint32_t, uint32_t, uint32_t, uint32_t);
+	probe qp_deletekey_done(void *, void *, bool);
+	probe qp_deletekey_start(void *, void *);
+	probe qp_deletename_done(void *, void *, void *);
+	probe qp_deletename_start(void *, void *);
+	probe qp_getkey_done(void *, void *, bool);
+	probe qp_getkey_start(void *, void *);
+	probe qp_getname_done(void *, void *, void *);
+	probe qp_getname_start(void *, void *);
+	probe qp_insert_done(void *, void *, uint32_t);
+	probe qp_insert_start(void *, void *, uint32_t);
+	probe qp_lookup_done(void *, void *, void *, void *, bool, bool);
+	probe qp_lookup_start(void *, void *, void *, void *);
+	probe qp_reclaim_chunks_done(void *, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t);
+	probe qp_reclaim_chunks_start(void *);
+	probe qp_recycle_done(void *, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t);
+	probe qp_recycle_start(void *);
+
+	probe qpmulti_marksweep_done(void *, void *, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t);
+	probe qpmulti_marksweep_start(void *, void *);
+
+	probe qpmulti_txn_lockedread(void *, void *);
+	probe qpmulti_txn_query(void *, void *);
+	probe qpmulti_txn_snapshot(void *, void *);
+	probe qpmulti_txn_update(void *, void *);
+	probe qpmulti_txn_write(void *, void *);
+	probe qpmulti_txn_commit_done(void *, void *);
+	probe qpmulti_txn_commit_start(void *, void *);
+	probe qpmulti_txn_rollback_done(void *, void *, uint32_t);
+	probe qpmulti_txn_rollback_start(void *, void *);
+
 	probe xfrin_axfr_finalize_begin(void *, char *);
 	probe xfrin_axfr_finalize_end(void *, char *, int);
 	probe xfrin_connected(void *, char *, int);
