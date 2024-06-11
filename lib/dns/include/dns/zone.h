881954cafa767b7ee3d1d6c269fda56a90d88ac3
Aram Sargsyan <aram@isc.org>
2024-06-11 10:55:18 +0000

    Implement rndc retransfer -force
    With this new optional argument if there is an ongoing zone
    transfer it will be aborted before a new zone transfer is scheduled.

--- lib/dns/include/dns/zone.h
+++ lib/dns/include/dns/zone.h
@@ -2002,6 +2002,15 @@ dns_zonemgr_set_tlsctx_cache(dns_zonemgr_t	*zmgr,
  */
 
 void
+dns_zone_stopreload(dns_zone_t *zone);
+/*%<
+ *      Stop the ongoing reload (active zone transfer) of specified zone if any.
+ *
+ * Requires:
+ *\li      'zone' to be a valid zone.
+ */
+
+void
 dns_zone_forcereload(dns_zone_t *zone);
 /*%<
  *      Force a reload of specified zone.
